5e86b67c2f67b8d81ae986c6061d26af6fa73125
murali-mac <murali@groupmail.asia>
2016-02-16 01:05:14 +0800

    app.js changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -54,4 +54,7 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-app.listen(app.get('port'));
+app.listen(app.get('port'),
+    function() {
+      console.log("Express server listening on port " + app.get('port'))
+    });
